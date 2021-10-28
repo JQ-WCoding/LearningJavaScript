c257cf1fca5967e985b2b079f29a2ff73cd90716
JunQ <junlee36520620@gmail.com>
2021-10-28 10:39:07 +0900

    chapter1

--- src/chapter1/main.js
+++ src/chapter1/main.js
@@ -5,8 +5,29 @@ $(document).ready(function () {
     paper.install(window);
     paper.setup(document.getElementById('mainCanvas'));
     // TODO : 그림 그리는 코드가 들어갈 자리
-    var c = Shape.Circle(200, 200, 50);
-    c.fillColor = 'green';
+    // let c;
+    // for (let x = 25; x < 400; x += 50) {
+    //     for (let y = 25; y < 400; y += 50) {
+    //         c = Shape.Circle(x, y, 20);
+    //         c.fillColor = 'red';
+    //     }
+    // }
+    const tool = new Tool();
+
+    const c = Shape.Circle(200, 200, 80);
+    c.fillColor = 'black';
+    let text = new PointText(200, 200);
+    text.justification = 'center';
+    text.fillColor = 'white';
+    text.fontSize = 20;
+    text.content = 'hello world';
+
+    tool.onMouseDown = function (event) {
+        // const c = Shape.Circle(event.point.x, event.point.y, 20);
+        const c = Shape.Circle(event.point, 20); // 간략하게 포인트
+        c.fillColor = 'red';
+    }
+
     paper.view.draw();
 
     console.log("main.js loaded");
